ea20838bd26aa31b8de7ceebd3bc85765ecd9c01
e-doschechnikova <evadrobakova@gmail.com>
2022-03-06 21:27:58 +0300

    add the finished version of the project

--- Form Validator Intro Project/script.js
+++ Form Validator Intro Project/script.js
@@ -16,7 +16,7 @@ function showSuccess(input) {
   formControl.className = "form-control success";
 }
 
-function isValidEmail(email) {
+function checkEmail(input) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
@@ -36,6 +36,28 @@ function checkRequired(inputArr) {
   });
 }
 
+function checkLenght(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, "Passwords do not match");
+  }
+}
+
 function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
@@ -44,4 +66,8 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLenght(username, 3, 15);
+  checkLenght(password, 6, 25);
+  checkEmail(email);
+  checkPasswordsMatch(password, password2);
 });
