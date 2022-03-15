226aec5b5d8ec7df5730955ada0d7e806b258bb7
e-doschechnikova <evadrobakova@gmail.com>
2022-03-15 19:18:56 +0300

    add js

--- Exchange Rate Calculator/script.js
+++ Exchange Rate Calculator/script.js
@@ -1,7 +1,25 @@
+const currencyElOne = document.getElementById("currency-one");
+const amountElOne = document.getElementById("amount-one");
+const currencyElTwo = document.getElementById("currency-two");
+const amountElTwo = document.getElementById("amount-two");
+
+const rateEl = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
 function calculate() {
-  fetch("items.json")
+  const currencyOne = currencyElOne.value;
+  const currencyTwoe = currencyElTwo.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest${currencyOne}`)
     .then((res) => res.json())
-    .then((data) => (document.body.innerHTML = data[0].text));
+    .then((data) => {
+      console.log(data);
+    });
 }
+
+currencyElOne.addEventListener("change", calculate);
+amountElOne.addEventListener("input", calculate);
+currencyElTwo.addEventListener("change", calculate);
+amountElTwo.addEventListener("input", calculate);
 
 calculate();
